00a0151f6e0a03a917e849b36e3a681460d1525d
Ryan Charles Smith <ryan-smith@comcast.net>
2019-11-19 00:50:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
+<<<<<<< HEAD
   get '/' do
       "Hello, World!"
   end
 
+=======
+  gets '/' do 
+    "200"
+  end
+
+ gets '/' do 
+    "Hello, world!"
+  end
+>>>>>>> 70dc42603bacd1a38f17b1d339aefeee5bd4ed40
 end
